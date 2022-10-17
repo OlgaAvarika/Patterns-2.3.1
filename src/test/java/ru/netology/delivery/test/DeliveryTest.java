501e70a871d2151a759394a149b2f9adf078d8c3
Ольга Евстифеева <olga1989nikol@rambler.ru>
2022-10-17 17:03:43 +0300

    Files fixed and commited

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.delivery.test;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,7 +8,9 @@ import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
 
-import static com.codeborne.selenide.Condition.exactText;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -22,24 +25,26 @@ class DeliveryTest {
     @Test
     @DisplayName("Should successful plan and replan meeting")
     void shouldSuccessfulPlanAndReplanMeeting() {
+        Configuration.holdBrowserOpen = true;
         var validUser = DataGenerator.Registration.generateUser("ru");
         var daysToAddForFirstMeeting = 4;
         var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
         var daysToAddForSecondMeeting = 7;
         var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        $x("//*[@data-test-id='city']//self::input").sendKeys(Keys.LEFT_CONTROL + "A" + Keys.DELETE);
-        $x("//*[@data-test-id='city']//self::input").setValue(DataGenerator.generateCity());
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE, firstMeetingDate);
-        $x("//*[@data-test-id='name']//self::input").sendKeys(Keys.LEFT_CONTROL + "A" + Keys.DELETE);
-        $x("//*[@data-test-id='name']//self::input").setValue(validUser.getName());
-        $x("//*[@data-test-id='phone']//self::input").sendKeys(Keys.LEFT_CONTROL + "A" + Keys.DELETE);
-        $x("//*[@data-test-id='phone']//self::input").setValue(validUser.getPhone());
-        $x("//*[@data-test-id='agreement']").click();
-        $x("//*[@class='button__text']").click();
-        $x("//*[@data-test-id='success-notification']").shouldHave(exactText("Успешно!\n" + "Встреча успешно запланирована на " + firstMeetingDate));
-        $x("//*[@data-test-id='date']//self::input").doubleClick().sendKeys(Keys.DELETE, secondMeetingDate);
-        $x("//*[@class='button__text']").click();
-        $("[data-test-id='replan-notification'] button").click();
-        $x("//*[@data-test-id='success-notification']").shouldHave(exactText("Успешно!\n" + "Встреча успешно запланирована на " + secondMeetingDate));
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE, firstMeetingDate);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='agreement']").click();
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $(".notification__content").shouldBe(appear).shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE, firstMeetingDate);
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $x("//span[contains(text(),'Перепланировать')]").shouldBe(appear, Duration.ofSeconds(3)).click();
+        $(".notification__content").shouldBe(appear).shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate));
+        $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE, secondMeetingDate);
+        $x("//*[contains(text(),'Запланировать')]").click();
+        $x("//span[contains(text(),'Перепланировать')]").click();
+        $(".notification__content").shouldBe(appear).shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
